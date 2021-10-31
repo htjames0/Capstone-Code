4de5e580c15d1d92b8113c27bce3a821594415d2
j4mag <j4mag@hotmail.com>
2021-10-31 15:53:42 -0500

    commiting

--- Sensors/motor2.py
+++ Sensors/motor2.py
@@ -8,34 +8,24 @@ import time
 
 def Servo_Initialize():
    pca = PCA9685()
-   pca.set_pwm_freq(60)
+   pca.set_pwm_freq(100)
    return pca
 
 
 def Motor_StartUp(pca):
-    print('Starting Motor Start Up Sequence')
-    #Motor_Speed(pca, 0)
-    pca.set_pwm(11, 0, math.floor(.15*4095))
+    print('Starting Motor Start Up sequence')
+    Motor_Speed(pca, 0)
     print('Neutral')
     time.sleep(15)
-
-    pca.set_pwm(11, 0, math.floor(.2*4095))
+    Motor_Speed(pca, 1)
     print('Forward')
-    time.sleep(3)
-
-    pca.set_pwm(11, 0, math.floor(.15*4095))
-    print('Neutral')
-    time.sleep(3)
-
-    pca.set_pwm(11, 0, math.floor(.1*4095))
+    time.sleep(6)
+    Motor_Speed(pca, -1)
     print('Reverse')
-    time.sleep(3)
-
-    pca.set_pwm(11, 0, math.floor(.15*4095))
+    time.sleep(6)
+    Motor_Speed(pca, 0)
     print('Neutral')
-    time.sleep(3)
-
-    print('Start Up Complete')
+    time.sleep(6)
 
 def Motor_Speed(pca, percent):
     #speed = math.floor(((percent)*3276) + 65535 * 0.15)
@@ -44,29 +34,27 @@ def Motor_Speed(pca, percent):
     elif percent == -1:
         speed = 0.1*4095
     elif percent == 0:
-        speed = 0.15*495
+        speed = 0.15*4095
     else: 
-        speed = percent * 65535
-
+        speed = percent * 4095
     speed = math.floor(speed)
     pca.set_pwm(0, 0, speed)
     print(speed)
 
 
 pca = Servo_Initialize()
-Motor_StartUp(pca)
+#Motor_StartUp(pca)
 
 print('')
 print('Changing Speeds:')
-pca.set_pwm(11,0,math.floor(.25*4095))
-time.sleep(10)
-pca.set_pwm(11,0,math.floor(.15*4095))
-
-
-#Motor_Speed(pca, 0.5)
+for i in range(0,100):
+    Motor_Speed(pca,i*.05)
+    time.sleep(5)
+    
+#Motor_Speed(pca, 0.25)
 #Motor_Speed(pca, -.12)
 #time.sleep(10)
-#Motor_Speed(pca, 0.1)
+#Motor_Speed(pca, 0.13)
 #time.sleep(10)
 #Motor_Speed(pca, 0)
 
