c345eee812d05d2ffd30982ac4459ffa3ae858dc
Hugh Tillman James Jr <hughtillmanjamesjr.@Hughs-MacBook-Pro.local>
2021-12-16 16:26:25 -0600

    Contains all final ROS, test, and Globe code

---
+++ Final/TestScripts/linetrackAD2.py
@@ -0,0 +1,89 @@
+import sys
+import time
+import argparse
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+import math
+import RPi.GPIO as GPIO
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BOARD)
+
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+
+
+#configuring argparse for commandline arguments
+parser = argparse.ArgumentParser(description='Data for this program.')
+parser.add_argument('--tim', action='store', type=int, default=10, help='time for program to run record sensor readings')
+parser.add_argument('--period', action='store', type=float, default = 0.25, help='sample period')
+parser.add_argument('--debug', action='store_true', help='specifies if debug statements are printed')
+args = parser.parse_args()
+
+if args.debug:
+  print(f'arguments: {vars(args)}')
+
+
+#functions
+
+#def readTrack(j=5):
+#    IR = [0]*j
+#    for i in range(j):
+#        IR[i] = mcp.read_adc(i)
+#    return IR
+
+
+
+# Hardware SPI configuration:
+#SPI_PORT   = 0
+#SPI_DEVICE = 0
+#mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+cs =  digitalio.DigitalInOut(board.D5)
+mcp = MCP.MCP3008(spi, cs)
+
+
+def readTrack2():
+    IR = [0]*5
+    IR[0] = AnalogIn(mcp, MCP.P0).value
+    IR[1] = AnalogIn(mcp, MCP.P1).value
+    IR[2] = AnalogIn(mcp, MCP.P2).value
+    IR[3] = AnalogIn(mcp, MCP.P3).value
+    IR[4] = AnalogIn(mcp, MCP.P4).value
+
+    offon = [0]*5
+    for i in range(5):
+        if IR[i] > 15000:
+            offon[i] = 0
+        if IR[i] < 15000:
+            offon[i] = 1
+    return offon, IR 
+
+start_time = time.time()
+cur_time = start_time
+mesg_time = start_time
+
+while (start_time + args.tim > cur_time):
+    time.sleep(0.01)
+    cur_time = time.time()
+    if(mesg_time + args.period < cur_time):
+      mesg_time = cur_time
+      t = round(cur_time- start_time,2)
+      #print(AnalogIn(mcp, MCP.P0).value)
+      offon, IR = readTrack2()
+      #w = 0*IR[0] + 1000*IR[1] + 2000*IR[2] + 3000*IR[3] + 4000*IR[4]
+      #s = IR[0] + IR[1] + IR[2] + IR[3] + IR[4]
+      #y = round(w/s,3) 
+      #print('Y = ')
+      #print(y)
+      print('IR is:')
+      print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4}'.format(*IR))
+      print('offon is:')
+      print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4}'.format(*offon))
+      #print(f"{t}s:\tTMP={TMP_Val},\tRES={RES_Val}")
+      #print(f"{t}\tRES={RES_Val}")
+
+#GPIO.cleanup()
---
+++ Final/TestScripts/steer.py
@@ -0,0 +1,46 @@
+
+#!/usr/bin/env python
+import math
+from board import SCL, SDA
+import busio
+from Adafruit_PCA9685 import PCA9685
+import time
+
+
+def Servo_Initialize():
+   pca = PCA9685()
+   pca.set_pwm_freq(100)
+   return pca
+
+def Wheel_Steer(pca, angle):
+   if angle > 180:
+      angle = 180
+   if angle < 0:
+      angle = 0
+   print(angle)
+   duty = math.floor(((angle/180)*270)+540)
+   pca.set_pwm(7, 0, duty)
+
+
+pca = Servo_Initialize()
+
+servo_min = 450 #hard right
+servo_max = 810 #hard left
+servo_middle = math.floor((servo_max - servo_min)/2) #middle
+
+print('The hard right PWM is 540')
+print('The hard left PWM is 810')
+print('The middle PWM is: %d' % servo_middle)
+
+while True:
+   pca.set_pwm(7, 0, 450)
+   #Wheel_Steer(pca, 180)
+   time.sleep(1)
+   pca.set_pwm(7, 0, 850)
+   #Wheel_Steer(pca, 0)
+   time.sleep(1)
+
+#for i in range(700, 1500, 20):
+   #pca.set_pwm(7, 0, i)
+   #time.sleep(1) 
+   #print(i)
